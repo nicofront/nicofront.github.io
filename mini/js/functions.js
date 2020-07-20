a41000801e259dd6285e080b989943d4c837ac01
Nico Perez <nicolasperez@multiplica.com>
2020-07-20 00:31:54 -0400

    Mini - minor tweaks (transitions and font-sizes)

--- mini/js/functions.js
+++ mini/js/functions.js
@@ -4,13 +4,12 @@ $(document).ready(function() {
 
   $('.acordeon-trigger').click(function(event){
   	event.preventDefault();
-  	console.log('Click');
   	var self = $(this).parents('.acordeon');
   	var next = self.next();
   	self.removeClass("acordeon-enabled");
   	self.addClass("acordeon-disabled");
     self.find('.acordeon-body:first').hide();
-    next.find('.acordeon-body:first').slideDown('slow');
+    next.find('.acordeon-body:first').slideDown(700);
   	next.removeClass("acordeon-disabled");
   	next.addClass("acordeon-enabled");
   });
@@ -24,7 +23,7 @@ $(document).ready(function() {
     self.find('.acordeon-body:first').hide();
     first.removeClass('acordeon-disabled');
     first.addClass('acordeon-enabled');
-    first.find('.acordeon-body:first').slideDown('slow');
+    first.find('.acordeon-body:first').slideDown(700);
   });
 
 });
