3daca01038f7a9d17d1355f188fd969a44fc051e
Nico Perez <nicolasperez@multiplica.com>
2020-07-20 00:17:41 -0400

    Mini - js acordeon

--- mini/js/functions.js
+++ mini/js/functions.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
 
+  $('.acordeon-enabled').find('.acordeon-body:first').show();
+
   $('.acordeon-trigger').click(function(event){
   	event.preventDefault();
   	console.log('Click');
-  	var self = this;
+  	var self = $(this).parents('.acordeon');
   	var next = self.next();
   	self.removeClass("acordeon-enabled");
   	self.addClass("acordeon-disabled");
+    self.find('.acordeon-body:first').hide();
+    next.find('.acordeon-body:first').slideDown('slow');
   	next.removeClass("acordeon-disabled");
   	next.addClass("acordeon-enabled");
+  });
+
+  $('.acordeon-reset').click(function(event){
+    event.preventDefault();
+    var self = $(this).parents('.acordeon');
+    var first = $('.wrapper').find('.acordeon:first');
+    self.removeClass("acordeon-enabled");
+    self.addClass("acordeon-disabled");
+    self.find('.acordeon-body:first').hide();
+    first.removeClass('acordeon-disabled');
+    first.addClass('acordeon-enabled');
+    first.find('.acordeon-body:first').slideDown('slow');
   });
 
 });
